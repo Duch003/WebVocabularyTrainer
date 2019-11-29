1ac31ad23b5e9b8570d777626492cc3e922a457f
Duch003 <tomaszmankin003@gmail.com>
2019-11-29 13:13:14 +0100

    Working on gameService

--- API/WebVocabularyTrainer/RestApi/Controllers/GameController.cs
+++ API/WebVocabularyTrainer/RestApi/Controllers/GameController.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using NLog;
 using RestApi.Services;
 
 namespace RestApi.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class GameController : Controller
     {
         private readonly GameService _gameService;
@@ -19,11 +22,31 @@ namespace RestApi.Controllers
             _gameService = gameService;
         }
 
-
+        [HttpGet]
         public async Task<IActionResult> GetSettingsForm()
         {
-            var output = await _gameService.GetSettingsForm();
-            return View();
+            _logger.Info($"[{IPService.GetSenderIPAddress(this)}] Requested settings form.");
+            var result = await _gameService.GetSettingsForm().ConfigureAwait(false);
+            if (result.IsFine)
+            {
+                _logger.Info($"[{IPService.GetSenderIPAddress(this)}] Output: default settings form / (200).");
+                return Ok(result.Output);
+            }
+            _logger.Error(result.Exception, $"[{IPService.GetSenderIPAddress(this)}] Output: {result.Exception.Message} / (500).");
+            return StatusCode(500);
+        }
+
+        public async Task<IActionResult> Perform()
+        {
+            _logger.Info($"[{IPService.GetSenderIPAddress(this)}] Requested settings form.");
+            var result = await _gameService.GetSettingsForm().ConfigureAwait(false);
+            if (result.IsFine)
+            {
+                _logger.Info($"[{IPService.GetSenderIPAddress(this)}] Output: default settings form / (200).");
+                return Ok(result.Output);
+            }
+            _logger.Error(result.Exception, $"[{IPService.GetSenderIPAddress(this)}] Output: {result.Exception.Message} / (500).");
+            return StatusCode(500);
         }
     }
 }
